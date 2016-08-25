7ed3621c067577c0b8a8a26b96de9b837b5ef1af
John N Blanchard <jnblanchard@mac.com>
2016-08-25 10:54:02 -0700

    Initial Commit

---
+++ RedditYoutubeMovies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RedditYoutubeMovies
+//
+//  Created by John N Blanchard on 8/25/16.
+//  Copyright © 2016 John N Blanchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RedditYoutubeMovies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RedditYoutubeMovies
+//
+//  Created by John N Blanchard on 8/25/16.
+//  Copyright © 2016 John N Blanchard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
